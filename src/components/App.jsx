dfe52067dffd71a2eed677d06d1a46fb9cdd1dac
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-04-28 19:37:45 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,27 +15,23 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-      />
-      <Statistics
+      {/* <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      /> */}
+       <Statistics
         title="Upload stats"
         stats={data}
       />
-      <Statistics
-      stats={data}
-      />
-      <FriendList
+      {/*<FriendList
       friends={friends}
       />
       <TransactionHistory
       items={transactions}
-      />
-      ; ;
+      /> */}
     </div>
   );
 };
@@ -44,12 +40,7 @@ export const Profile = () => {
   return (
     <div class="profile">
       <div class="description">
-        <img
-          src={user.avatar}
-          alt="User avatar"
-          class="avatar"
-          width="300"
-        />
+        <img src={user.avatar} alt="User avatar" class="avatar" width="100" />
         <p class="name">{user.username}</p>
         <p class="tag">{user.tag}</p>
         <p class="location">{user.location}</p>
@@ -74,42 +65,35 @@ export const Profile = () => {
 };
 
 export const Statistics = () => {
-  return (
-    <section class="statistics">
-      <h2 class="title">{data[0].stats}</h2>
-
-      <ul class="stat-list">
+  return data.map(
+    stats =>
+   (
+   <section class="statistics">
+     {/*  <h2 class="title">Upload stats</h2>*/}
+      <ul class="stat-list"> 
         <li class="item">
-          <span class="label">{data[0].label}</span>
-          <span class="percentage">{data[0].percentage}</span>
+          <span class="label">{stats.label}</span>
+          <span class="percentage">{stats.percentage}</span>
         </li>
-        {/* <li class="item">
-          <span class="label">.mp3</span>
-          <span class="percentage">14%</span>
-        </li>
-        <li class="item">
-          <span class="label">.pdf</span>
-          <span class="percentage">41%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp4</span>
-          <span class="percentage">12%</span>
-        </li> */}
       </ul>
     </section>
-  );
+  ))
 };
 
 export const FriendList = () => {
   return (
     <ul class="friend-list">
-  
-
-    <li class="item">
-      <span class="status">{friends[0].isOnline}</span>
-      <img class="avatar" src={friends[0].avatar} alt="User avatar" width="48" />
-      <p class="name">{friends[0].name}</p>
-    </li></ul>
+      <li class="item">
+        <span class="status">{friends[0].isOnline}</span>
+        <img
+          class="avatar"
+          src={friends[0].avatar}
+          alt="User avatar"
+          width="48"
+        />
+        <p class="name">{friends[0].name}</p>
+      </li>
+    </ul>
   );
 };
 
