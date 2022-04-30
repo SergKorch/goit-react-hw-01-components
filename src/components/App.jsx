7a2da6cb95ce6a2bddc059543ca8a39adecae952
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-04-30 23:07:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,9 +27,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics stats={data} />
+      {/* <Statistics stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
